924e5a8d58e82b220854c99ec96998029fdeade5
Mimioj101 <mimioj@gmail.com>
2020-12-18 17:16:56 -0500

    if/else statement

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,10 +25,18 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
    
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+
+    if #draw
+      false
+    elsif #board empty
+      false
+    elsif position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     else
       false
     end
+
   end
 end
